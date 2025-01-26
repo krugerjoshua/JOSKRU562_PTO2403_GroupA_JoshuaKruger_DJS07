77ed7bb9ead23ed7a3bb7c20f988b6c4fb6abce3
Joshua <joshuakruger66@live.com>
2025-01-26 21:04:50 +0200

    Followed along with the scrimba tutorial to create  a meme generator using React. Added some styling  to to fit my personal taste.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import React from 'react'
+import Header from '../components/header'
+import Meme from '../components/meme'
 
-function App() {
-
+export default function App() {
   return (
-    <>
-     
-    </>
+    <div>
+      <Header />
+      <Meme />
+    </div>
   )
 }
-
-export default App
